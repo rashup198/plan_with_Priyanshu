20ec796af9066b9af2e8c425734f08586b8ceec3
Priyanshu Pandey <priyanshu.pandey2021@vitstudent.ac.in>
2023-06-02 22:47:30 +0530

    updated

--- src/components/Card.js
+++ src/components/Card.js
@@ -17,13 +17,13 @@ function Card({id, image, info, price,name, removeTour}){
         <div className="tour-info">
  
             <div className="tour-details">
-                <h4 className="tour-price">{price}</h4>
+                <h4 className="tour-price">₹ {price}</h4>
                 <h4 className="tour-name">{name}</h4>
             </div>
             <div className="description">
                 {description}
                 <span className="read-more" onClick={readmoreHandler}>
-                {readmore ? `show less`: `readmore`}
+                {readmore ? `  Show less`: `Read More`}
                 </span>
             </div>
         </div>
