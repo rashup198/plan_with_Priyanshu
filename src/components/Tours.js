18f09e0b30cbfab49b378d84c2192a18a414ea96
Priyanshu Pandey <priyanshu.pandey2021@vitstudent.ac.in>
2023-06-02 20:34:02 +0530

    Plan_With Pandey

---
+++ src/components/Tours.js
@@ -0,0 +1,16 @@
+
+
+function Tours(){
+    return(
+        <div>
+            <div>
+                <h2>Plan with Priyanshu</h2>
+            </div>
+            <div>
+                
+            </div>
+        </div>
+    )
+}
+
+export default Tours;
